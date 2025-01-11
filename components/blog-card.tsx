afd5dc88bc2d749059f530be97c7b6559284e427
lihoubrak <lihoub@metfone.kh>
2025-01-11 10:55:41 +0700

    update feature

--- components/blog-card.tsx
+++ components/blog-card.tsx
@@ -18,18 +18,20 @@ export const BlogCard: React.FC<BlogCardProps> = ({
   image,
   description,
 }) => (
-  <Card className="w-full sm:w-[240px] bg-white shadow-sm rounded-lg overflow-hidden hover:scale-105 transition-all ease-in-out duration-300">
+  <Card className="w-full sm:w-[240px] md:w-[300px] lg:w-[320px] bg-white shadow-sm rounded-lg overflow-hidden hover:scale-105 transition-all ease-in-out duration-300">
     <CardContent className="p-0">
       {/* Image for the blog post */}
-      <Image
-        src={image}
-        alt={title}
-        width={300}
-        height={150}
-        className="rounded-t-md mb-4 object-cover"
-      />
+      <div className="relative w-full h-40 mb-4">
+        <Image
+          src={image}
+          alt={title}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-t-md"
+        />
+      </div>
 
-      <CardTitle className="font-semibold text-sm text-gray-900 mb-2 px-4 line-clamp-1 ">
+      <CardTitle className="font-semibold text-sm text-gray-900 mb-2 px-4 line-clamp-1">
         {title}
       </CardTitle>
       <p className="text-xs text-gray-600 line-clamp-3 px-4 mb-4">
